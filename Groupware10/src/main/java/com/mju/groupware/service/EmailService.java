d1bfd4e0e2ee884f2de81ff8e909e3753b51ec9c
dlgusdn978 <dlgusdn978@naver.com>
2023-11-20 20:50:11 +0900

    Feat : Edit File Directory

--- Groupware10/src/main/java/com/mju/groupware/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.mju.groupware.service;
-
-import com.mju.groupware.dto.User;
-
-public interface EmailService {
-	public void sendEmail(User user);
-	
-	public boolean authNum(String authNum);
-}
--- Groupware2/src/main/webapp/resources/js/login.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#submit").click(function(){
-    if($("#mid").val().length==0 || $("#pw").val().length==0) {
-      alert("필수항목이 입력되지 않았습니다.");
-      return false;
-    }
-  });
-});
